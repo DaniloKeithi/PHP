fceb4920e4bcbc0d3297f1db3fccb141fe4d71e1
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2024-08-27 09:46:44 -0300

    Add files via upload

--- exercicio/conexao.php
+++ exercicio/conexao.php
@@ -4,14 +4,12 @@
     $senha = '';
     $banco = 'etecmcm';
 
-    $conexao = new mysqli($host, $usuario, $senha, $banco);
+    $conexao = new mysqli($host,$usuario,$senha,$banco);
 
-    if($conexao ->connect_error){
-        die('Falha de conexão:' . $conexao -> connect_error);
-
+    if($conexao->connect_error){
+        die('Falha de conexão: ' . $conexao->connect_error);
     }
-
-    //else{
-    //    echo "CONECTADO COM SUCESSO";
-    //}
+    // else{
+    //     echo "CONECTADO COM SUCESSO!";
+    // }
 ?>
